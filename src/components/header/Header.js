5ed227e5b7c9278466a68ea8c18d7f820b102b54
Kwarn <karlwarner.dev@gmail.com>
2021-08-15 11:55:09 +0100

    background added to header to allow contrast to build navbar against - to save my eyes from a white screen

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { StyledHeaderWrapper } from "./HeaderStyles";
+import backgroundImage from "../../assets/Header_bg.jpg";
 
 export default function Header() {
-  return <StyledHeaderWrapper></StyledHeaderWrapper>;
+  return (
+    <StyledHeaderWrapper background={backgroundImage}></StyledHeaderWrapper>
+  );
 }
--- src/components/header/HeaderStyles.js
+++ src/components/header/HeaderStyles.js
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const StyledHeaderWrapper = styled.div``;
+export const StyledHeaderWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-image: ${(props) => `url(${props.background})`};
+  background-size: cover;
+  background-position: right;
+`;
