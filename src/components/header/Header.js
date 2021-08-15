1580212c1e64f33c8e81c059e791649ff126ce34
Kwarn <karlwarner.dev@gmail.com>
2021-08-15 13:58:18 +0100

    Header: content & styles added. Mobile layout complete.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { StyledHeaderWrapper } from "./HeaderStyles";
+import {
+  LeftContentBlock,
+  StyledHeaderContainer,
+  StyledHeaderWrapper,
+  StyledDescription,
+  StyledTitle,
+  TimeDateInfoContainer,
+  RightContentBlock,
+} from "./HeaderStyles";
 import backgroundImage from "../../assets/Header_bg.jpg";
+import ticketImage from "../../assets/Ticket.png";
+import { ReactComponent as XIcon } from "../../assets/X Icon-1.svg";
+import { ReactComponent as Calendar } from "../../assets/Calendar.svg";
+import { ReactComponent as Pin } from "../../assets/Pin.svg";
 
 export default function Header() {
   return (
-    <StyledHeaderWrapper background={backgroundImage}></StyledHeaderWrapper>
+    <StyledHeaderWrapper background={backgroundImage}>
+      <StyledHeaderContainer>
+        <LeftContentBlock>
+          <XIcon />
+          <StyledTitle>INSIDER STORIES FROM PRODUCT PEOPLE</StyledTitle>
+          <StyledDescription>
+            A single-day digital product and experience conference sharing
+            actionable insights from makers, to makers.
+          </StyledDescription>
+          <TimeDateInfoContainer>
+            <div>
+              <Calendar style={{ minWidth: "50px" }} />
+              <h2>8th October 2021</h2>
+            </div>
+            <div>
+              <Pin style={{ minWidth: "50px" }} />
+              <h2>Cineworld, Birmingham Venue takeover</h2>
+            </div>
+          </TimeDateInfoContainer>
+        </LeftContentBlock>
+        <RightContentBlock>
+          <img src={ticketImage}></img>
+        </RightContentBlock>
+      </StyledHeaderContainer>
+    </StyledHeaderWrapper>
   );
 }
--- src/components/header/HeaderStyles.js
+++ src/components/header/HeaderStyles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledHeaderWrapper = styled.div`
   width: 100%;
@@ -6,4 +6,67 @@ export const StyledHeaderWrapper = styled.div`
   background-image: ${(props) => `url(${props.background})`};
   background-size: cover;
   background-position: right;
+  /* overflow: hidden; */
+`;
+
+export const StyledHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto 20px;
+`;
+
+const ContentBlock = css`
+  // margin offset to accomodate absolute positioned navbar
+  margin: 20vh 20px auto auto;
+  color: white;
+  width: 100%;
+  height: auto;
+  font-family: "Righteous";
+`;
+
+export const LeftContentBlock = styled.div`
+  ${ContentBlock}
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  text-align: left;
+`;
+
+export const StyledTitle = styled.h2`
+  margin: 15px auto;
+  word-wrap: wrap;
+  font-size: 40px;
+  font-weight: bolder;
+`;
+
+export const StyledDescription = styled.p`
+  margin: 20px auto;
+  font-size: 22px;
+  line-height: 25px;
+`;
+
+export const TimeDateInfoContainer = styled.div`
+  div {
+    margin: 25px auto auto 0;
+    display: flex;
+    flex-direction: row;
+    h2 {
+      margin: auto 10px;
+    }
+  }
+  margin: 25px auto auto 0;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const RightContentBlock = styled.div`
+  ${ContentBlock}
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+    margin: auto;
+  }
 `;
